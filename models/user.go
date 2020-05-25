02bac7342589c8a2b206eba7ab933ec85bb93986
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-25 14:20:32 +0300

    Implement the model layer

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*User is awesome.*/
 type User struct {
 	ID        int
@@ -12,13 +17,47 @@ var (
 	nextID = 1
 )
 
-func getUsers() []*User {
+// GetUsers is absolutely awesome
+func GetUsers() []*User {
 	return users
 }
 
-func addUser(u User) (User, error) {
+// AddUser is absolutely awesome
+func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("don't set user ID or set to 0")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("user with ID %v not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("user with ID %v not found", u.ID)
+}
+
+func RemoveUserById(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("user with ID %v not found", id)
 }
